ba87ab76fe6d6fd80a52e729328cef9bc2c6993f
Biswajit Biswas <jfbiswajit@gmail.com>
2021-05-13 15:10:20 +0600

    Add: S2E5

---
+++ app.js
@@ -0,0 +1,8 @@
+// Modules
+const fs = require('fs');
+const path = require('path');
+const appDir = path.dirname(require.main.filename);
+
+const text = fs.readFileSync('./Asset/input.txt', 'utf-8');
+
+fs.writeFileSync('./Asset/output.txt', 'kfkkfk');
