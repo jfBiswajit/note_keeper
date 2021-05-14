fae73059ee2c09e8f242af141052d51109487ceb
Biswajit Biswas <jfbiswajit@gmail.com>
2021-05-14 13:21:34 +0600

    Add: S2E10

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const http = require('http');
+const fs = require('fs');
+
+// This is a synchronous code which is declare as top level code and execute only once
+const apiData = fs.readFileSync('data/products.json', 'utf-8');
 
 const server = http.createServer((req, res) => {
   if (req.url == '/home' || req.url == '/') {
     res.end('This is home page.');
-  }else if (req.url == '/blog') {
-    res.end('This is blog page.');
-  }else if (req.url == '/about') {
-    res.end('This is about page.');
+  }else if (req.url == '/api') {
+    res.writeHead(200, {
+      'Content-Type': 'Application/Json',
+    });
+   res.end(apiData);
   } else {
-    res.writeHead(404, {
-      'user-name': 'Biswajit Biswas'
-    })
-    
-    res.end('Sorry! page not found.');
+    res.writeHead(404);
+    res.end('Page not found!');
   }
-});
+})
 
 server.listen(8000, '127.0.0.1', () => {
-  console.log('Server started at http://127.0.0.1:8000');
-});
+   console.log('Server started at http://127.0.01:8000');
+})
